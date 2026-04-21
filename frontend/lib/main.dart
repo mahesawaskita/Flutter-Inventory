d5743eff88ee121329b6080468f690fed042a69e
pael <productionrafael49@gmail.com>
2026-04-21 22:09:13 +0700

    bebas

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screen/user/status_barang_user.dart';
+import 'package:frontend/screen/user/dashboard_user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const StatusBarangUserScreen(),
+      home: const DashboardUserScreen (),
     );
   }
 }
