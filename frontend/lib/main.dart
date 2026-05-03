c4ad60cfdc24f0af1131d6324b9b2c6c05c62000
Mahesa <mahesawaskita@gmail.com>
2026-05-03 21:08:59 +0700

    Mau Kerjain Sostem Loginl

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:frontend/screen/user/dashboard_user.dart';
-=======
-import 'package:frontend/sistem_login/login_page.dart';
->>>>>>> 948292890770667acdc553f21055e39f91e8412f
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-<<<<<<< HEAD
       home: const DashboardUserScreen (),
-=======
-      home: const LoginPage (),
->>>>>>> 948292890770667acdc553f21055e39f91e8412f
     );
   }
 }
