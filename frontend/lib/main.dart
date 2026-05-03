a51288b3ad041cc4cbe02e0fbcdea4ed9159d76b
pael <productionrafael49@gmail.com>
2026-05-03 20:14:46 +0700

    bebas

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screen/user/daftar_barang_user.dart';
+import 'package:frontend/screen/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const DaftarBarangUserScreen (),
+      home: const LoginPage(),
     );
   }
 }
